2bb51f26f34eabc2f5b8bc8e018aee1043f376d6
veedahv <veedahv55@gmail.com>
2020-10-02 10:58:00 +0100

    new commit

---
+++ js/popup.js
@@ -0,0 +1,41 @@
+function toggle() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    let popup1 = document.getElementById('popup1');
+    popup1.classList.toggle('active')
+}
+
+function toggle2() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    let popup2 = document.getElementById('popup2');
+    popup2.classList.toggle('active')
+}
+
+function toggle3() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    let popup3 = document.getElementById('popup3');
+    popup3.classList.toggle('active')
+}
+
+function toggle4() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    let popup4 = document.getElementById('popup4');
+    popup4.classList.toggle('active')
+}
+
+function toggle5() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    let popup5 = document.getElementById('popup5');
+    popup5.classList.toggle('active')
+}
+
+function toggle6() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    let popup6 = document.getElementById('popup6');
+    popup6.classList.toggle('active')
+}
